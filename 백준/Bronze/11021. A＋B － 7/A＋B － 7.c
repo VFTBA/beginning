abdd8706221a4032699cde3c617c2b688be6042f
VFTBA <121803304+VFTBA@users.noreply.github.com>
2023-01-08 09:20:49 +1000

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	int t;
+	int a, b, c = 0;
+	scanf("%d", &t);
+
+	for (int i = 1; i <= t; i++) {
+		scanf("%d %d", &a, &b);
+		c = a + b;
+		printf("Case #%d: %d\n", i, c);
+	}
+}
