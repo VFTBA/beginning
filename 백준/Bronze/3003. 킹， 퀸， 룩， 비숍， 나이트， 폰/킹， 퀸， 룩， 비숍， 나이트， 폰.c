eb07a9cc449882e995b71d842f2313af920d27df
VFTBA <121803304+VFTBA@users.noreply.github.com>
2023-01-17 09:03:51 +1000

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.c
@@ -0,0 +1,283 @@
+#include <stdio.h>
+int main() {
+	int k, q, r, b, kn, p;
+	scanf("%d %d %d %d %d %d", &k, &q, &r, &kn, &b, &p);
+
+	if (k == 0) {
+		k = k + 1;
+	}
+
+
+	else if (k == 1) {
+		k = 0;
+	}
+
+	else if (k == 2) {
+		k = 1 - k;
+	}
+
+	else if (k == 3) {
+		k = 1 - k;
+	}
+
+	else if (k == 4) {
+		k = 1 - k;
+	}
+
+	else if (k == 5) {
+		k = 1 - k;
+	}
+
+	else if (k == 6) {
+		k = 1 - k;
+	}
+
+	else if (k == 7) {
+		k = 1 - k;
+	}
+
+	else if (k == 8) {
+		k = 1 - k;
+	}
+
+	else if (k == 9) {
+		k = 1 - k;
+	}
+
+	else if (k == 10) {
+		k = 1 - k;
+	}
+
+	if (q == 0) {
+		q = q + 1;
+
+	}
+
+
+	else if (q == 1) {
+		q = 0;
+	}
+
+	else if (q == 2) {
+		q = 1 - q;
+	}
+
+	else if (q == 3) {
+		q = 1 - q;
+	}
+
+	else if (q == 4) {
+		q = 1 - q;
+	}
+
+	else if (q == 5) {
+		q = 1 - q;
+	}
+
+	else if (q == 6) {
+		q = 1 - q;
+	}
+
+	else if (q == 7) {
+		q = 1 - q;
+	}
+
+	else if (q == 8) {
+		q = 1 - q;
+	}
+
+	else if (q == 9) {
+		q = 1 - q;
+	}
+
+	else if (q == 10) {
+		q = 1 - q;
+	}
+
+	if (r == 0) {
+		r = r + 2;
+
+	}
+
+
+	else if (r == 1) {
+		r = 1;
+	}
+
+	else if (r == 2) {
+		r = r - 2;
+	}
+
+	else if (r == 3) {
+		r = 2 - r;
+	}
+
+	else if (r == 4) {
+		r = 2 - r;
+	}
+
+	else if (r == 5) {
+		r = 2 - r;
+	}
+
+	else if (r == 6) {
+		r = 2 - r;
+	}
+
+	else if (r == 7) {
+		r = 2 - r;
+	}
+
+	else if (r == 8) {
+		r = 2 - r;
+	}
+
+	else if (r == 9) {
+		r = 2 - r;
+	}
+
+	else if (r == 10) {
+		r = 2 - r;
+	}
+
+	if (kn == 0) {
+		kn = kn + 2;
+
+	}
+
+
+	else if (kn == 1) {
+		kn = 1;
+	}
+
+	else if (kn == 2) {
+		kn = kn - 2;
+	}
+
+	else if (kn == 3) {
+		kn = 2 - kn;
+	}
+
+	else if (kn == 4) {
+		kn = 2 - kn;
+	}
+
+	else if (kn == 5) {
+		kn = 2 - kn;
+	}
+
+	else if (kn == 6) {
+		kn = 2 - kn;
+	}
+
+	else if (kn == 7) {
+		kn = 2 - kn;
+	}
+
+	else if (kn == 8) {
+		kn = 2 - kn;
+	}
+
+	else if (kn == 9) {
+		kn = 2 - kn;
+	}
+
+	else if (kn == 10) {
+		kn = 2 - kn;
+	}
+
+	if (b == 0) {
+		b = b + 2;
+	}
+
+
+	else if (b == 1) {
+		b = 1;
+	}
+
+	else if (b == 2) {
+		b = b - 2;
+	}
+
+	else if (b == 3) {
+		b = 2 - b;
+	}
+
+	else if (b == 4) {
+		b = 2 - b;
+	}
+
+	else if (b == 5) {
+		b = 2 - b;
+	}
+
+	else if (b == 6) {
+		b = 2 - b;
+	}
+
+	else if (b == 7) {
+		b = 2 - b;
+	}
+
+	else if (b == 8) {
+		b = 2 - b;
+	}
+
+	else if (b == 9) {
+		b = 2 - b;
+	}
+
+	else if (b == 10) {
+		b = 2 - b;
+	}
+
+	if (p == 0) {
+		p = p + 8;
+	}
+
+
+	else if (p == 1) {
+		p = 8 - p;
+	}
+
+	else if (p == 2) {
+		p = 8 - p;
+	}
+
+	else if (p == 3) {
+		p = 8 - p;
+	}
+
+	else if (p == 4) {
+		p = 8 - p;
+	}
+
+	else if (p == 5) {
+		p = 8 - p;
+	}
+
+	else if (p == 6) {
+		p = 8 - p;
+	}
+
+	else if (p == 7) {
+		p = 8 - p;
+	}
+
+	else if (p == 8) {
+		p = 8 - p;
+	}
+
+	else if (p == 9) {
+		p = 8 - p;
+	}
+
+	else if (p == 10) {
+		p = 8 - p;
+	}
+
+	printf("%d %d %d %d %d %d", k, q, r, kn, b, p);
+}
+
+	
+
