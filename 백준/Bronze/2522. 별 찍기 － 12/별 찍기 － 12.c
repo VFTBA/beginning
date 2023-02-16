d00d8c4682cf1bc500537d6c606dcff299393c7e
VFTBA <121803304+VFTBA@users.noreply.github.com>
2023-02-16 18:14:13 +1000

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	if (n >= 1 && n <= 100) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = n-i; j > 0; j--) {
+				printf(" ");
+			}
+
+			for (int k = 1; k <= i; k++) {
+				printf("*");
+			}
+
+			printf("\n");
+		}
+
+		for (int i = n - 1; i  > 0; i--) {
+			for (int j = n-i; j > 0; j--) {
+				printf(" ");
+			}
+
+			for (int k = 1; k <= i; k++) {
+				printf("*");
+			}
+
+			printf("\n");
+		}
+        return 0;
+	}
+}
