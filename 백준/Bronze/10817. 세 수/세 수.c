c62e7fe244a5c21c376d69ce7f31ca69cf103911
VFTBA <121803304+VFTBA@users.noreply.github.com>
2023-02-17 10:13:47 +1000

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+
+	if (a >= b && a >= c && b >= c) {
+		printf("%d", b);
+	}
+
+	else if (a >= b && a >= c && c >= b) {
+		printf("%d", c);
+	}
+
+	else if (b >= a && b >= c && a >= c) {
+		printf("%d", a);
+	}
+
+	else if (b >= a && b >= c && c >= a) {
+		printf("%d", c);
+	}
+
+	else if (c >= a && c >= b && b >= a) {
+		printf("%d", b);
+	}
+
+	else if (c >= a && c >= b && a >= b) {
+		printf("%d", a);
+        
+	}
+}
