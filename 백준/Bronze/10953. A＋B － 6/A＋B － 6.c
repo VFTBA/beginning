3a4370cbc006b12267c1fd99fb3f7f387190e2ee
VFTBA <121803304+VFTBA@users.noreply.github.com>
2023-02-25 22:03:46 +1000

    [Bronze II] Title: A+B - 6, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+	int n;
+	int a, b;
+	int c = 0;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d,%d", &a, &b);
+		c = a + b;
+		printf("%d\n", c);
+		
+	}
+}
