61aa363a6dce98641ed184fea45d08cdbaf8f008
VFTBA <121803304+VFTBA@users.noreply.github.com>
2023-03-20 17:24:06 +1000

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	if (n == 0 || n == 1) {
+		if (n == 0) {
+			printf("YONSEI");
+		}
+
+		else if (n == 1) {
+			printf("Leading the Way to the Future");
+		}
+	}
+}
