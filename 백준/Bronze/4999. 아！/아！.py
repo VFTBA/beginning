4eab7e493bc5c0571bb6b27985e339056cfb0c5b
VFTBA <121803304+VFTBA@users.noreply.github.com>
2023-04-05 21:53:08 +1000

    [Bronze V] Title: 아!, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.py
@@ -0,0 +1,11 @@
+first = str(input())
+second = str(input())
+first_length = len(first)
+second_length = len(second)
+
+if first_length >= second_length:
+     print("go")
+else:
+     print("no")
+
+        
